04eb21d4b0180a8d160a7adaf54d64a1f948c86a
jorober <72576743+jorober@users.noreply.github.com>
2020-11-12 19:48:49 +0000

    percentage calculator

---
+++ Module 3/scripts/task3.3.1.js
@@ -0,0 +1,14 @@
+function percentageCalculator(number, percentage){
+    
+    var percentageOf = (number * percentage) / 100;
+    //BODMAS
+
+    return percentageOf;
+
+}
+
+//document.write (percentageCalculator(100,25));
+
+var result = PercentageCalculator(100, 25);
+
+document.write(result);

720ce407d105a3f103892338440610ed01421b71
jorober <72576743+jorober@users.noreply.github.com>
2020-11-24 21:09:50 +0000

    with an event listener

---
+++ Module 3/scripts/task3.6.1.1.js
@@ -0,0 +1,19 @@
+
+                                                                        //Select One
+
+
+var paragraph = document.querySelector('p');
+  paragraph.style.backgroundColor= 'red';
+  paragraph.style.color ='white';
+  paragraph.style.fontFamily ='Verdana';
+  paragraph.style.border = '20px solid blue'
+
+  var heading = document.querySelector('h1');
+  heading.style.color = 'blue';
+
+var imgCake = document.getElementById('meh');
+imgCake.src;
+imgCake.src = 'https://images.pexels.com/photos/5340255/pexels-photo-5340255.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+
+
+ 
